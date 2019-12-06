652a0927650bbd994a6cc4c3e3fe38a7730ab96e
madelinezec <mez2113@columbia.edu>
2019-12-06 17:11:46 -0500

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const { promisify } = require("util");
-const exec = promisify(require("child_process").exec);
-const fs = require("fs");
-const { MongoClient } = require("mongodb");
+const { promisify } = require('util');
+const exec = promisify(require('child_process').exec);
+const fs = require('fs');
+const { MongoClient } = require('mongodb');
 
 function insertJob(payloadObj, jobTitle, jobUserName, jobUserEmail) {
   const dbName = process.env.DB_NAME;
@@ -13,7 +13,7 @@ function insertJob(payloadObj, jobTitle, jobUserName, jobUserEmail) {
     title: jobTitle,
     user: jobUserName,
     email: jobUserEmail,
-    status: "inQueue",
+    status: 'inQueue',
     createdTime: new Date(),
     startTime: null,
     endTime: null,
@@ -22,7 +22,7 @@ function insertJob(payloadObj, jobTitle, jobUserName, jobUserEmail) {
     failures: [],
     result: null,
     payload: payloadObj,
-    logs: {}
+    logs: {},
   };
 
   // we are looking for jobs in the queue with the same payload
@@ -32,31 +32,26 @@ function insertJob(payloadObj, jobTitle, jobUserName, jobUserEmail) {
 
   const uri = `mongodb+srv://${username}:${secret}@cluster0-ylwlz.mongodb.net/test?retryWrites=true&w=majority`;
   const client = new MongoClient(uri, { useNewUrlParser: true });
-  client.connect(err => {
+  client.connect((err) => {
     if (err) {
-      console.log("error connecting to Mongo");
-      return;
+      console.log('error connecting to Mongo');
+      return false;
     }
     const collection = client.db(dbName).collection(collName);
     collection.updateOne(filterDoc, updateDoc, { upsert: true }).then(
-      result => {
+      (result) => {
         if (result.upsertedId) {
-          console.log(
-            "You successfully enqued a staging job to docs autobuilder. This is the record id: ",
-            result.upsertedId
-          );
-          return result.upsertedId;
+          console.log('You successfully enqued a staging job to docs autobuilder. This is the record id: ', result.upsertedId);
+          return [true, result.upsertedId];
         }
-        console.log("Already existed ", newJob);
-        return "Already Existed";
+        console.log('Already existed ', newJob);
+        return 'Already Existed';
       },
-      error => {
-        console.log(
-          "There was an error enqueing a staging job to docs autobuilder. Here is the error: ",
-          error
-        );
-        return error;
+      ((error) => {
+        console.log('There was an error enqueing a staging job to docs autobuilder. Here is the error: ', error);
+        return [false, error];
       }
+      ),
     );
     client.close();
   });
@@ -69,12 +64,12 @@ function createPayload(
   urlArg,
   patchArg,
   buildSizeArg,
-  lastCommit
+  lastCommit,
 ) {
   const payload = {
-    jobType: "githubPush",
-    source: "github",
-    action: "push",
+    jobType: 'githubPush',
+    source: 'github',
+    action: 'push',
     repoName: repoNameArg,
     branchName: branchNameArg,
     isFork: true,
@@ -84,7 +79,7 @@ function createPayload(
     url: urlArg,
     newHead: lastCommit,
     buildSize: buildSizeArg,
-    patch: patchArg
+    patch: patchArg,
   };
 
   return payload;
@@ -92,31 +87,31 @@ function createPayload(
 
 async function getBranchName() {
   return new Promise((resolve, reject) => {
-    exec("git rev-parse --abbrev-ref HEAD", (error, stdout) => {
+    exec('git rev-parse --abbrev-ref HEAD', (error, stdout) => {
       if (error !== null) {
         console.log(`exec error: ${error}`);
         reject(error);
       }
-      resolve(stdout.replace("\n", ""));
+      resolve(stdout.replace('\n', ''));
     });
   });
 }
 
 // extract repo name from url
 function getRepoName(url) {
-  let repoName = url.split("/");
+  let repoName = url.split('/');
   repoName = repoName[repoName.length - 1];
-  repoName = repoName.replace(".git", "");
-  repoName = repoName.replace("\n", "");
+  repoName = repoName.replace('.git', '');
+  repoName = repoName.replace('\n', '');
   return repoName;
 }
 
 // delete patch file
 async function deletePatchFile() {
   return new Promise((resolve, reject) => {
-    exec("rm myPatch.patch", error => {
+    exec('rm myPatch.patch', (error) => {
       if (error !== null) {
-        console.log("exec error deleting patch file: ", error);
+        console.log('exec error deleting patch file: ', error);
         reject(error);
       }
       resolve();
@@ -125,13 +120,13 @@ async function deletePatchFile() {
 }
 async function getRepoInfo() {
   return new Promise((resolve, reject) => {
-    exec("git config --get remote.origin.url", (error, stdout) => {
+    exec('git config --get remote.origin.url', (error, stdout) => {
       if (error !== null) {
         console.log(`exec error: ${error}`);
         reject(error);
       }
 
-      const repoUrl = stdout.replace("\n", "");
+      const repoUrl = stdout.replace('\n', '');
       resolve(repoUrl);
     });
   });
@@ -139,12 +134,12 @@ async function getRepoInfo() {
 
 async function getGitEmail() {
   return new Promise((resolve, reject) => {
-    exec("git config --global user.email", (error, stdout) => {
+    exec('git config --global user.email', (error, stdout) => {
       if (error !== null) {
         console.log(`exec error: ${error}`);
         reject(error);
       } else {
-        resolve(stdout.replace("\n", ""));
+        resolve(stdout.replace('\n', ''));
       }
     });
   });
@@ -152,12 +147,12 @@ async function getGitEmail() {
 
 async function getGitUser() {
   return new Promise((resolve, reject) => {
-    exec("git config --global user.name", (error, stdout) => {
+    exec('git config --global user.name', (error, stdout) => {
       if (error !== null) {
         console.log(`exec error: ${error}`);
         reject(error);
       } else {
-        resolve(stdout.replace("\n", ""));
+        resolve(stdout.replace('\n', ''));
       }
     });
   });
@@ -165,18 +160,16 @@ async function getGitUser() {
 
 async function getGitCommits() {
   return new Promise((resolve, reject) => {
-    exec("git cherry", (error, stdout) => {
+    exec('git cherry', (error, stdout) => {
       if (error !== null) {
         console.log(`exec error: ${error}`);
         reject(error);
       } else {
-        const cleanedup = stdout.replace(/\+ /g, "");
+        const cleanedup = stdout.replace(/\+ /g, '');
         const commitarray = cleanedup.split(/\r\n|\r|\n/);
         commitarray.pop(); // remove the last, dummy element that results from splitting on newline
         if (commitarray.length === 0) {
-          console.log(
-            "You have tried to create a staging job from local commits but you have no committed work. Please make commits and then try again"
-          );
+          console.log('You have tried to create a staging job from local commits but you have no committed work. Please make commits and then try again');
           reject();
         }
         if (commitarray.length === 1) {
@@ -192,16 +185,17 @@ async function getGitCommits() {
     });
   });
 }
-async function getGitPatchFromLocal(branchName) {
+async function getGitPatchFromLocal() {
+  
   return new Promise((resolve, reject) => {
-    exec(`git diff origin/test2 --ignore-submodules > myPatch.patch`, error => {
+    exec(`git diff origin/test2 --ignore-submodules > myPatch.patch`, (error) => {
       if (error !== null) {
-        console.log("error generating patch: ", error);
+        console.log('error generating patch: ', error);
         reject(error);
       } else {
-        fs.readFile("myPatch.patch", "utf8", (err, data) => {
+        fs.readFile('myPatch.patch', 'utf8', (err, data) => {
           if (err) {
-            console.log("error reading patch file: ", err);
+            console.log('error reading patch file: ', err);
             reject(err);
           }
           resolve(data);
@@ -213,17 +207,18 @@ async function getGitPatchFromLocal(branchName) {
 async function getGitPatchFromCommits(firstCommit, lastCommit) {
   //need to delete patch file?
   return new Promise((resolve, reject) => {
+    
     if (lastCommit === null) {
-      console.log("last commit is null!!!");
-      const patchCommand = "git show HEAD > myPatch.patch";
-      exec(patchCommand, error => {
+      console.log("last commit is null!!!")
+      const patchCommand = 'git show HEAD > myPatch.patch';
+      exec(patchCommand, (error) => {
         if (error !== null) {
-          console.log("error generating patch: ", error);
+          console.log('error generating patch: ', error);
           reject(error);
         } else {
-          fs.readFile("myPatch.patch", "utf8", (err, data) => {
+          fs.readFile('myPatch.patch', 'utf8', (err, data) => {
             if (err) {
-              console.log("error reading patch file", err);
+              console.log('error reading patch file', err);
               reject(err);
             }
             resolve(data);
@@ -231,18 +226,15 @@ async function getGitPatchFromCommits(firstCommit, lastCommit) {
         }
       });
     } else {
-      const patchCommand = `git diff ${firstCommit}^...${lastCommit} > myPatch.patch`;
-      //console.log(`bad: git diff ${firstCommit}^...${lastCommit} > myPatch.patch`)
-      //const patchCommand =  "git diff " + firstCommit + "^..." + lastCommit + " > myPatch.patch";
-      console.log(`good: `, patchCommand);
-      exec(patchCommand, error => {
+      const patchCommand = `git diff ${firstCommit}^...${lastCommit} > myPatch.patch`
+      exec(patchCommand, (error) => {
         if (error !== null) {
-          console.log("error generating patch: ", error);
+          console.log('error generating patch: ', error);
           reject(error);
         } else {
-          fs.readFile("myPatch.patch", "utf8", (err, data) => {
+          fs.readFile('myPatch.patch', 'utf8', (err, data) => {
             if (err) {
-              console.log("error reading patch file ", err);
+              console.log('error reading patch file ', err);
               reject(err);
             }
             resolve(data);
@@ -256,23 +248,21 @@ async function getGitPatchFromCommits(firstCommit, lastCommit) {
 function validateConfiguration() {
   const missingConfigs = [];
 
-  if (process.env.DB_NAME === undefined || process.env.DB_NAME === "") {
-    missingConfigs.push("DB_NAME");
+  if (process.env.DB_NAME === undefined || process.env.DB_NAME === '') {
+    missingConfigs.push('DB_NAME');
   }
-  if (process.env.COL_NAME === undefined || process.env.COL_NAME === "") {
-    missingConfigs.push("COL_NAME");
+  if (process.env.COL_NAME === undefined || process.env.COL_NAME === '') {
+    missingConfigs.push('COL_NAME');
   }
-  if (process.env.USERNAME === undefined || process.env.USERNAME === "") {
-    missingConfigs.push("USERNAME");
+  if (process.env.USERNAME === undefined || process.env.USERNAME === '') {
+    missingConfigs.push('USERNAME');
   }
-  if (process.env.SECRET === undefined || process.env.SECRET === "") {
-    missingConfigs.push("SECRET");
+  if (process.env.SECRET === undefined || process.env.SECRET === '') {
+    missingConfigs.push('SECRET');
   }
   if (missingConfigs.length !== 0) {
-    console.log(
-      `The ~/.config/.snootyenv file is found but does not contain the following required fields: ${missingConfigs.toString()}`
-    );
-    process.exit();
+    console.log(`The ~/.config/.snootyenv file is found but does not contain the following required fields: ${missingConfigs.toString()}`);
+    process.exit()
   }
 }
 
@@ -283,23 +273,19 @@ async function main() {
   validateConfiguration();
 
   if (patchFlag === undefined) {
-    console.log(
-      'You need a patch flag("commit" or "local") in your make command'
-    );
+    console.log('You need a patch flag("commit" or "local") in your make command');
     return;
   }
 
   let invalidFlag = false;
 
-  if (buildSize !== undefined && buildSize !== "repo") {
+  if (buildSize!== undefined && buildSize !== 'repo') {
     console.log('Invalid build size. Use "world" or "repo"');
     invalidFlag = true;
   }
 
-  if (patchFlag !== "local" && patchFlag !== "commit") {
-    console.log(
-      'Invalid patch flag. Use "commit" to stage a build from the committed work you have locally or use "local" to stage a build from the uncommitted work you have locally'
-    );
+  if (patchFlag !== 'local' && patchFlag !== 'commit') {
+    console.log('Invalid patch flag. Use "commit" to stage a build from the committed work you have locally or use "local" to stage a build from the uncommitted work you have locally');
     invalidFlag = true;
   }
 
@@ -312,9 +298,9 @@ async function main() {
   const url = await getRepoInfo();
   const repoName = getRepoName(url);
   const branchName = await getBranchName();
-  const newHead = "genericscsss";
+  const newHead = 'genericscsss';
   // toggle btwn create patch from commits or what you have saved locally
-  if (patchFlag === "commit") {
+  if (patchFlag === 'commit') {
     const { firstCommit, lastCommit } = await getGitCommits();
     const patch = await getGitPatchFromCommits(firstCommit, lastCommit);
     const payLoad = await createPayload(
@@ -324,18 +310,17 @@ async function main() {
       url,
       patch,
       buildSize,
-      newHead
+      newHead,
     );
-    const success = insertJob(
+    insertJob(
       payLoad,
       `Github Push: ${userName}/repoName`,
       userName,
-      userEmail
+      userEmail,
     );
-    return success;
   }
 
-  if (patchFlag === "local") {
+  if (patchFlag === 'local') {
     const patch = await getGitPatchFromLocal(branchName);
     const payLoad = await createPayload(
       repoName,
@@ -344,16 +329,16 @@ async function main() {
       url,
       patch,
       buildSize,
-      newHead
+      newHead,
     );
-    const success = insertJob(
+    insertJob(
       payLoad,
       `Github Push: ${userName}/repoName`,
       userName,
-      userEmail
+      userEmail,
     );
-    return success;
   }
+
   await deletePatchFile();
 }
 
