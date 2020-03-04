a3cd7727032e0a828584c3197eca49287125e80f
madelinezec <mez2113@columbia.edu>
2020-03-04 16:22:49 -0500

    test

--- stagingUtils.js
+++ stagingUtils.js
@@ -134,6 +134,9 @@ module.exports = {
     });
   },
   async checkIfPrivateRepo(url) {
+
+    let cleanedURL = url.replace('.git', '');
+    cleanedURL = cleanedURL.replace('\n', '');
     console.log(' called with this url: ', url)
     return new Promise((resolve, reject) => {
       exec(`curl ${url} --head > visibility.txt`)
