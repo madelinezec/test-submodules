7c331932283550b0eb31e4e38aa959b24aa2f584
madelinezec <maddie.zechar@mongodb.com>
2020-07-13 11:12:00 -0400

    updategit add stagingUtils.js

--- stagingUtils.js
+++ stagingUtils.js
@@ -231,7 +231,7 @@ module.exports = {
 
   async getGitPatchFromLocal(upstreamBranchName) {
     return new Promise((resolve, reject) => {
-      exec(`git diff HEAD --ignore-submodules > myPatch.patch`)
+      exec(`git diff master...${upstreamBranchName} --ignore-submodules > myPatch.patch`)
         .then(() => {
           fs.readFile('myPatch.patch', 'utf8', (err, data) => {
             if (err) {
@@ -308,5 +308,5 @@ module.exports = {
       console.error(err);
       throw err;
     }
-  }
+  },
 };
